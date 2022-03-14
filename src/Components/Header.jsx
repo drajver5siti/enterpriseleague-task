1e99e45d33fdcb14f6ecefb56f6f0fab3da1da9a
drajver5siti <ivan.pavlovskii@outlook.com>
2022-03-14 14:54:56 +0100

    Logo change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import {GiHamburgerMenu} from 'react-icons/gi';
 import {IoIosArrowDown} from 'react-icons/io'
-import logo from '../images/logo.svg';
 
 const Header = () => {
 
@@ -23,7 +22,7 @@ const Header = () => {
     return(
         <div className={`z-10 flex  w-full mx-auto h-16 items-center justify-between text-primary pt-2 px-10 md:px-24 sticky top-0 bg-white ${scrolled && 'drop-shadow-sm'}`}>
                 <ul className='flex  justify-between w-4/12'>
-                    <img src={logo} alt="logo" />
+                    <img src='https://enterpriseleague.com/public/img/logo-header.svg' alt="logo" />
                     <li className='hidden  md:flex items-center px-3 hover:cursor-pointer hover:opacity-90'>Product <IoIosArrowDown className='mt-1 ml-1'/></li>
                     <li className='hidden md:flex items-center px-3 hover:cursor-pointer hover:opacity-90'>Resources <IoIosArrowDown className='mt-1 ml-1'/></li>
                     <li className='hidden md:flex items-center px-3 hover:cursor-pointer hover:opacity-90'>Company <IoIosArrowDown className='mt-1 ml-1'/></li>
